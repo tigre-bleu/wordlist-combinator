a6bb408491fe0b70d8be4dcab3a3892448214b1a
Antoine Vacher <antoine.vacher@tigre-bleu.net>
2021-04-23 09:11:48 +0200

    Set a minimum and a maximum number for how many words to combine

--- wordlist-combinator.py
+++ wordlist-combinator.py
@@ -4,7 +4,8 @@ import itertools
 import argparse
 
 parser = argparse.ArgumentParser(description='Combine a list of words in a list of combined words with separators')
-parser.add_argument('-wc', '--word-count', type=int, help='How many words to combine together. Default is 3', nargs='?', default=3)
+parser.add_argument('-min', '--min-words', type=int, help='Minimum words to combine together. Default is 3', nargs='?', default=3)
+parser.add_argument('-max', '--max-words', type=int, help='Maximum words to combine together. Default is 3', nargs='?', default=3)
 parser.add_argument('-s', '--separators', metavar='N', type=str, nargs='?', help='Separators between combined words. Default is \"-_\". Empty separator is always included.', default=["-","_"])
 parser.add_argument('file')
 args = parser.parse_args()
@@ -22,5 +23,6 @@ separators = [char for char in args.separators]
 separators.append("")
 
 for separator in separators:
-    for element in itertools.permutations(words, args.word_count):
-        print(separator.join(element))
+    for words_count in range(args.min_words, args.max_words + 1):
+        for element in itertools.permutations(words, words_count):
+            print(separator.join(element))
