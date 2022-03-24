0336d101fc23732267144481240259b9c5680f20
Tigre-Bleu <devel@tigre-bleu.net>
2022-03-24 15:38:02 +0100

    Add option to combine words from multiple files

--- README.md
+++ README.md
@@ -2,26 +2,37 @@
 
 Simple tool to merge different words into a single one with customized separators. This tool was designed to build password candidates from a dictionnary of words that could be part of the password.
 
-The tool will create all possible permutations of N words (N varying between a customized minimum and maximum. Default is 3) with all defined separators character (Default is no character, `-` and `_`).
+The tool can work in twi ways:
+
+**A single file is provided as parameter:**
+
+In that cas, the tool will create all possible permutations of N words (N varying between a customized minimum and maximum. Default is 3) with all defined separators character (Default is no character, `-` and `_`).
+
+**Multiple files are provided as parameter:**
+
+In that case the tool will create all the possible permutations with one word (and only one) from each file with all defined separators character (Default is no character, `-` and `_`). `-min` and `-max` parameters are not used in that case as the number of combined words is always equal to the number of files provided.
 
 # Usage
 
 ```
-usage: wordlist-combinator.py [-h] [-min [MIN_WORDS]] [-max [MAX_WORDS]] [-s [N]] [-o [OUTPUT_FILE]] file
+usage: wordlist-combinator.py [-h] [-min [MIN_WORDS]] [-max [MAX_WORDS]] [-s [N]] [-o [OUTPUT_FILE]]
+                              files [files ...]
 
-Combines a list of words in a list of combined words with separators
+Combines a list of words in a list of combined words with separators. Example: wordlist-combinator.py -min 2 -max 4
+-s "_-." -o merged.lst example.lst
 
 positional arguments:
-  file
+  files                 if single argument: combine all lines, if multiple arguments: combine lines from all files
 
 optional arguments:
   -h, --help            show this help message and exit
   -min [MIN_WORDS], --min-words [MIN_WORDS]
-                        Minimum words to combine together. Default is 3
+                        Minimum words to combine together (if single file is provided). Default is 3
   -max [MAX_WORDS], --max-words [MAX_WORDS]
-                        Maximum words to combine together. Default is 3
+                        Maximum words to combine together (if single file is provided). Default is 3
   -s [N], --separators [N]
                         Separators between combined words. Default is "-_". Empty separator is always included.
+                        Example: "_-.,"
   -o [OUTPUT_FILE], --output-file [OUTPUT_FILE]
                         Outputs to a file in addition to stdout
 ```
--- wordlist-combinator.py
+++ wordlist-combinator.py
@@ -5,29 +5,33 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Combines a list of words in a list of combined words with separators. \
         Example: wordlist-combinator.py -min 2 -max 4 -s \"_-.\" -o merged.lst example.lst')
-parser.add_argument('-min', '--min-words', type=int, help='Minimum words to combine together. Default is 3', nargs='?', default=3)
-parser.add_argument('-max', '--max-words', type=int, help='Maximum words to combine together. Default is 3', nargs='?', default=3)
+parser.add_argument('-min', '--min-words', type=int, help='Minimum words to combine together (if single file is provided). Default is 3', nargs='?', default=3)
+parser.add_argument('-max', '--max-words', type=int, help='Maximum words to combine together (if single file is provided). Default is 3', nargs='?', default=3)
 parser.add_argument('-s', '--separators', metavar='N', type=str, nargs='?', help='Separators between combined words. Default is \"-_\". Empty separator is always included. Example: \"_-.,\"', default=["-","_"])
 parser.add_argument('-o', '--output-file', type=str, help='Outputs to a file in addition to stdout', nargs='?')
-parser.add_argument('file')
+parser.add_argument('files', help='if single argument: combine all lines, if multiple arguments: combine lines from all files', nargs='+')
 args = parser.parse_args()
 
-if args.min_words < 2:
-    print("Minimum words shall be greater or equals to 2.")
-    quit()
-if args.min_words > args.max_words:
-    print("Maximum words shall be greater or equal to minimum words.")
-    quit()
+if len(args.files) == 1:
+    if args.min_words < 2:
+        print("Minimum words shall be greater or equals to 2.")
+        quit()
+    if args.min_words > args.max_words:
+        print("Maximum words shall be greater or equal to minimum words.")
+        quit()
 
-# Open dictionary file
-words = []
-try:
-    with open(args.file) as fp:
-        for line in fp:
-            words.append(line.rstrip())
-except FileNotFoundError as not_found:
-    print("Error: File " + not_found.filename + " not found")
-    quit()
+# Open dictionary files
+dicts = []
+for f in args.files:
+    words = []
+    try:
+        with open(f) as fp:
+            for line in fp:
+                words.append(line.rstrip())
+        dicts.append(words)
+    except FileNotFoundError as not_found:
+        print("Error: File " + not_found.filename + " not found")
+        quit()
 
 # Open output file if required
 if args.output_file is not None:
@@ -41,14 +45,24 @@ separators = [char for char in args.separators]
 separators.append("")
 
 for separator in separators:
-    for words_count in range(args.min_words, args.max_words + 1):
-        for element in itertools.permutations(words, words_count):
-            candidate = separator.join(element)
-            print(candidate)
-            # Print to output file if required
-            if args.output_file is not None:
-                f.write(candidate + "\n")
-
+    if len(args.files) == 1 :
+        # We combine within the lines of a single file
+        for words_count in range(args.min_words, args.max_words + 1):
+            for element in itertools.permutations(words, words_count):
+                candidate = separator.join(element)
+                print(candidate)
+                # Print to output file if required
+                if args.output_file is not None:
+                    f.write(candidate + "\n")
+    else:
+        # We combine words from different files
+        for words in list(itertools.product(*dicts)):
+            for element in itertools.permutations(words):
+                candidate = separator.join(element)
+                print(candidate)
+                # Print to output file if required
+                if args.output_file is not None:
+                    f.write(candidate + "\n")
 
 if args.output_file is not None:
     f.close()
